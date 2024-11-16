ba3539628c19005968764f54dd2180afc4df2588
konstantin <konstabarilo12@gmail.com>
2024-11-16 12:37:49 +0300

    fix(client): Fix background for AuthLayout

--- client/src/pages/auth/layout/AuthLayout.tsx
+++ client/src/pages/auth/layout/AuthLayout.tsx
@@ -1,9 +1,14 @@
+import { ContentWrapper } from "@/shared/ui";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
     return (
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] sm:w-[460px] md:w-[600px] rounded-md shadow-lg border-[0.5px] border-gray-100">
-            <Outlet />
-        </div>
+        <main className="w-screen h-screen">
+            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] sm:w-[460px] md:w-[600px]">
+                <ContentWrapper>
+                    <Outlet />
+                </ContentWrapper>
+            </div>
+        </main>
     );
 }
